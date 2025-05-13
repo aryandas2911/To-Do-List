34c05766fe5c6d6cbb3b46ec55d261f58a2ce406
aryandas2911 <aryandas2911@gmail.com>
2025-05-13 17:19:29 +0530

    Added task removal button and enabled auto-save with preload of previous tasks using CSS and JS

--- script.js
+++ script.js
@@ -8,6 +8,30 @@ function AddTask() {
     let li = document.createElement("li");
     li.innerHTML = InputBox.value;
     ListContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
   InputBox.value = "";
+  saveData();
 }
+
+ListContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+});
+
+function saveData() {
+  localStorage.setItem("data", ListContainer.innerHTML);
+}
+
+function showData() {
+  ListContainer.innerHTML = localStorage.getItem("data");
+}
+
+showData();
